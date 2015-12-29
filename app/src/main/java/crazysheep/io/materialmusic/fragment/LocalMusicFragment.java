028b6841a7fa1712b9f2b53f7ec154d75e5017a1
crazysheep <ly513782705@gmail.com>
2015-12-29 23:32:30 +0800

    request READ_EXTERNAL_STORAGE for android 6.0 Marshmallow
    Signed-off-by: crazysheep <ly513782705@gmail.com>

--- app/src/main/java/crazysheep/io/materialmusic/fragment/LocalMusicFragment.java
+++ app/src/main/java/crazysheep/io/materialmusic/fragment/LocalMusicFragment.java
@@ -1,6 +1,8 @@
 package crazysheep.io.materialmusic.fragment;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -8,6 +10,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.anthonycr.grant.PermissionsManager;
+import com.anthonycr.grant.PermissionsResultAction;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -34,25 +39,47 @@ public class LocalMusicFragment extends BaseFragment {
         View contentView = inflater.inflate(R.layout.fragment_local_music, container, false);
         ButterKnife.bind(this, contentView);
 
-        initToolbar();
+        requestStoragePermission();
+        initUI();
+
+        return contentView;
+    }
+
+    private void requestStoragePermission() {
+        PermissionsManager.getInstance().requestPermissionsIfNecessaryForResult(getActivity(),
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                new PermissionsResultAction() {
+                    @Override
+                    public void onGranted() {
+                    }
+
+                    @Override
+                    public void onDenied(String permission) {
+                        getActivity().finish();
+                    }
+                });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        PermissionsManager.getInstance().notifyPermissionsChange(permissions, grantResults);
+    }
+
+    private void initUI() {
+        ((MainActivity)getActivity()).setToolbar(mToolbar);
+        if(!Utils.checkNull(getSupportActionBar())) {
+            getSupportActionBar().setTitle(getString(R.string.tv_local_music_title));
+            getSupportActionBar().setHomeButtonEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
 
         mMusicAdapter = new MusicPagerAdapter(getFragmentManager(), getActivity());
         mContentVp.setAdapter(mMusicAdapter);
         mContentVp.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
         mTabLayout.setupWithViewPager(mContentVp);
         mContentVp.setOffscreenPageLimit(mMusicAdapter.getCount());
-
-        return contentView;
-    }
-
-    private void initToolbar() {
-        ((MainActivity)getActivity()).setToolbar(mToolbar);
-
-        if(!Utils.checkNull(getSupportActionBar())) {
-            getSupportActionBar().setTitle(getString(R.string.tv_local_music_title));
-            getSupportActionBar().setHomeButtonEnabled(true);
-            getSupportActionBar().setDisplayShowHomeEnabled(true);
-        }
     }
 
 }
