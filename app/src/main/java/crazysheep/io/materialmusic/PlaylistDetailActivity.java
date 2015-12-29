366d6ec6406a5d1be0d537db21c50c1b2b6e4ee4
crazysheep <ly513782705@gmail.com>
2015-12-29 20:27:28 +0800

    use CollapsingToolbarLayout for parallax effect at PlaylistDetailActivity
    Signed-off-by: crazysheep <ly513782705@gmail.com>

--- app/src/main/java/crazysheep/io/materialmusic/PlaylistDetailActivity.java
+++ app/src/main/java/crazysheep/io/materialmusic/PlaylistDetailActivity.java
@@ -6,19 +6,16 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageView;
 
-import com.poliveira.parallaxrecyclerview.ParallaxRecyclerAdapter;
 import com.squareup.picasso.Picasso;
 
 import java.io.File;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import crazysheep.io.materialmusic.adapter.PlaylistDetailAdapter;
+import crazysheep.io.materialmusic.adapter.SongsAdapter;
 import crazysheep.io.materialmusic.bean.localmusic.LocalAlbumDto;
 import crazysheep.io.materialmusic.utils.Utils;
 
@@ -34,9 +31,9 @@ public class PlaylistDetailActivity extends BaseSwipeBackActivity {
     @Bind(R.id.appbar) AppBarLayout mAppbar;
     @Bind(R.id.toolbar) Toolbar mToolbar;
     @Bind(R.id.data_rv) RecyclerView mSongsRv;
-    private PlaylistDetailAdapter mAdapter;
+    @Bind(R.id.parallax_header_iv) ImageView mParallaxHeaderIv;
+    private SongsAdapter mAdapter;
     private LinearLayoutManager mLayoutMgr;
-    private ImageView mParallaxHeaderIv;
 
     private LocalAlbumDto mAlbumDto;
 
@@ -70,40 +67,19 @@ public class PlaylistDetailActivity extends BaseSwipeBackActivity {
             getSupportActionBar().setHomeButtonEnabled(true);
         }
 
-        // set appbar layout alpha 0f
-        alphaAppbar(0);
-
         mLayoutMgr = new LinearLayoutManager(this);
         mSongsRv.setLayoutManager(mLayoutMgr);
 
-        View parallaxHeader = LayoutInflater.from(this).inflate(
-                R.layout.layout_playlist_detail_parallax_header, mSongsRv, false);
-        mParallaxHeaderIv = ButterKnife.findById(parallaxHeader, R.id.parallax_header_iv);
         if(!TextUtils.isEmpty(mAlbumDto.album_cover))
             Picasso.with(this)
                     .load(new File(mAlbumDto.album_cover))
                     .into(mParallaxHeaderIv);
-        mAdapter = new PlaylistDetailAdapter(this, mAlbumDto.songs);
-        mAdapter.setParallaxHeader(parallaxHeader, mSongsRv);
-        mAdapter.setOnParallaxScroll(new ParallaxRecyclerAdapter.OnParallaxScroll() {
-            @Override
-            public void onParallaxScroll(float percentage, float offset, View parallax) {
-                if(!Float.isNaN(percentage)) {
-                    if (parallax.getBottom() < 10)
-                        percentage = 1f;
-                    alphaAppbar(Math.round(percentage * 255));
-                }
-            }
-        });
+        mAdapter = new SongsAdapter(this, mAlbumDto.songs);
         mSongsRv.setAdapter(mAdapter);
     }
 
     private void parseIntent() {
         mAlbumDto = getIntent().getParcelableExtra(EXTRA_ALBUM);
-    }
-
-    private void alphaAppbar(int alpha) {
-        mAppbar.getBackground().setAlpha(alpha);
     }
 
 }
